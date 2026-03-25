6727fbcbf8778c04e89ccb92e1a0e6da8cc47eff
Harshil-Desai <hdesai1633@gmail.com>
2026-03-25 10:38:28 +0530

    Update assetPrefix in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@/ui", "@/utils"],
-  assetPrefix: 'https://atomic-dev-tools.vercel.app',
+  assetPrefix: 'https://atomic-dev-tools-web.vercel.app',
 };
 
 module.exports = nextConfig;
